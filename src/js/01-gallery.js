9ad39d16c86a99dceeb6a40a5c917ca89b0beefe
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-07-31 17:15:08 +0200

    simpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const newLightBox = document.querySelector(".gallery");
+const images = galleryItems
+  .map(
+    (image) =>
+      ` <div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+      title="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+newLightBox.innerHTML = images;
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
+console.log(galleryItems);
 console.log(galleryItems);
