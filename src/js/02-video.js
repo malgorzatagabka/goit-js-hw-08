0e73863859c9f886a1474d16f4ddb4027cc9b276
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-08-02 19:09:28 +0200

    hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const LOCALSTORAGE_KEY = localStorage.getItem("videoplayer-current-time");
+console.log(LOCALSTORAGE_KEY);
+
+
+player.setCurrentTime(LOCALSTORAGE_KEY).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(function (time) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time.seconds));
+ 
+}, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(formInput, 500));
+form.addEventListener('submit', formSubmit);
+
+
+function formInput(event) {
+  event.preventDefault();
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify,
+    form.elements.message.value,
+    form.elements.email.value
+  );
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  localStorage.clear();
+}
+
+function formData() {
+  const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (savedMessage) {
+    input.value = savedMessage.email;
+    textarea.value = savedMessage.message;
+  }
+}
